a541e0ac041d02c507247d1ffe5232ee90578943
Baiyr628 <1143486861@qq.com>
2018-06-08 15:12:40 +0800

    修改首页

--- js/index.js
+++ js/index.js
@@ -1,16 +1,24 @@
 $(document).ready(function () {
-    $(window).on('resize', function (){
-        var width = $('body').width();
 
-        if( width > 411){
-            var img = $('.carousel-inner').find('img')
+    function change() {
+        var width = $('body').width();
+        var img = $('.carousel-inner').find('img');
+
+        if( width < 411){
             for (var i =0 ;i < img .length ; i++){
-                console.log(img[i])
                 var n = i+1;
-                // var src = "img/ " + n +".png";
-                $(img[i]).attr('src','data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==');
+                var src = "img/"+n+".png";
+                $(img[i]).attr('src',src);
+            }
+        }else {
+            for (var o =0 ;o < img .length ; o++){
+                $(img[o]).attr('src','data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==');
             }
         }
+    }
+    change();
+    $(window).on('resize', function (){
+        change()
     });
 
 });
